3b156dc917d535625e92bc040ef576dc1d47cb13
kelly rios <keldrios@soy.sena.edu.co>
2023-08-27 18:49:22 -0500

    conf(backend): add MapWhen Method

--- Lab01Kelly/Lab01Kelly/Program.cs
+++ Lab01Kelly/Lab01Kelly/Program.cs
@@ -3,14 +3,17 @@ using Lab01Kelly.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
+// Add services to the container.
+
+builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 
-// Add services to the container
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -22,8 +25,10 @@ else
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
@@ -44,27 +49,43 @@ app.UseAuthorization();
 //});
 
 app.Use(async (context, next) => {
-    Console.WriteLine($"Logic before executing the next delegate in the Use method");
-    await next.Invoke();
-    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+Console.WriteLine($"Logic before executing the next delegate in the Use method");
+await next.Invoke();
+Console.WriteLine($"Logic after executing the next delegate in the Use method");
 });
 
 app.Map("/usingmapbranch", builder => {
-    builder.Use(async (context, next) =>
-    {
-        Console.WriteLine("Map branch logic in the Use method before the next delegate");
-        await next.Invoke(); Console.WriteLine("Map branch logic in the Use method after the next delegate");
-    });
-    builder.Run(async context => {
-        Console.WriteLine($"Map branch response to the client in the Run method");
-        await context.Response.WriteAsync("Hello from the map branch.");
-    });
+builder.Use(async (context, next) =>
+{
+Console.WriteLine("Map branch logic in the Use method before the next delegate");
+await next.Invoke();
+Console.WriteLine("Map branch logic in the Use method after the next delegate");
+});
+builder.Run(async context => {
+Console.WriteLine($"Map branch response to the client in the Run method");
+await context.Response.WriteAsync("Hello from the map branch.");
+});
+});
+
+
+
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder => {
+builder.Run(async context => {
+await context.Response.WriteAsync("Hello from the MapWhen branch.");
+});
+});
+
+app.Run(async context => {
+Console.WriteLine($"Writing the response to the client in the Run method");
+await context.Response.WriteAsync("Hello from the middleware component.");
 });
 
 
 app.MapControllers();
 
 app.Run();
+
+
 
 namespace Microsoft.AspNetCore.Http
 {
